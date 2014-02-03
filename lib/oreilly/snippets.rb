13e75c8f367e377ddcde222402498680baa593f7
Chris Dawson <xrdawson@gmail.com>
2014-02-03 10:49:46 -0800

    Add specs

--- lib/oreilly/snippets.rb
+++ lib/oreilly/snippets.rb
@@ -2,6 +2,40 @@ require "oreilly/snippets/version"
 
 module Oreilly
   module Snippets
-    # Your code goes here...
+    def self.process( input, filename, language=nil, identifier=nil )
+      input
+    end
+    
+    def self.parse( input, offset=nil )
+      output = []
+      input.scan( /\[([^=]*)="([^"]*)",\s*([^=]*)="([^"]*)",\s*([^=]*)="([^"]*)"\]\n(.*)\n(.*)\n\7/mx ) do |m|
+        match = {}
+        3.times do |i|
+          match[m[i*2].to_sym] = m[(i*2)+1]
+        end
+        
+        match[:snippet] = m[6]
+        match[:throwaway] = m[7]
+        
+        output << match
+      end
+      output
+    end
   end
 end
+# To include this snippet in an AsciiDoc file, add the following block:
+
+#   [filename="factorial.js", language="js", identifier="FACTORIAL_FUNC"]
+# snippet~~~~
+#   Put any descriptive text you want here. It will be replaced with the
+# specified code snippet when you build ebook outputs
+# snippet~~~~
+#   When the ebook is generated, the following output will be present in place of the snippet block:
+
+#   function factorial(number) {
+#                if (number == 0) {
+#                    return 1
+#                  } else {
+#                    return factorial(number - 1) * number
+#                  }
+#   }
---
+++ spec/process_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+TEMPLATE = <<END
+
+ABC
+
+[filename="fixtures/factorial.js", language="js", identifier="FACTORIAL_FUNC"]
+snippet~~~~
+Put any descriptive text you want here. It will be replaced with the
+specified code snippet when you build ebook outputs
+snippet~~~~
+
+DEF
+
+END
+
+describe "#parse" do
+  it "should parse the file and extract the correct things" do
+    outputs = Oreilly::Snippets.parse( TEMPLATE )
+    output = outputs[0]
+    puts output.inspect
+    output[:filename].should == "fixtures/factorial.js"
+    output[:language].should == "js"
+    output[:identifier].should == "FACTORIAL_FUNC"
+    output[:throwaway].should match( /^Put any descriptive/ )
+    output[:snippet].should == "snippet~~~~"
+  end
+end
+
+describe "#process" do
+
+  it "should process a simple file" do
+    output = Oreilly::Snippets.process( TEMPLATE, "fixtures/", "asdas" )
+    output.should match( /ABC/ )
+    output.should match( /DEF/ )
+  end
+
+end
