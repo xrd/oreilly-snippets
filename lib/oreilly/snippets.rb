ad20c1f78dd018f97e024e80d1b3e1b2611cc39d
Chris Dawson <xrdawson@gmail.com>
2014-02-03 10:05:00 -0800

    Initial checkin

---
+++ lib/oreilly/snippets.rb
@@ -0,0 +1,7 @@
+require "oreilly/snippets/version"
+
+module Oreilly
+  module Snippets
+    # Your code goes here...
+  end
+end
---
+++ lib/oreilly/snippets/version.rb
@@ -0,0 +1,5 @@
+module Oreilly
+  module Snippets
+    VERSION = "0.0.1"
+  end
+end
